7736d199cf91fd71219ea87d3f6e6c02be3249e9
Michael <me@michaelbitokhov.com>
2018-04-22 02:16:03 +0000

    Add fish files

---
+++ .config/fish/functions/rpass.fish
@@ -0,0 +1,8 @@
+function rpass -a length -d "Output some random base64 password"
+    if test -z $length
+        set length 24
+    end
+
+    echo (head -c $length /dev/urandom | base64 -w 0)
+end
+
